d6addfeeeeb370b14ce1a98d22edd29292ae8ce1
shreyanshchauhan <shreyansh0514@gmail.com>
2025-04-02 11:57:58 +0530

    registration system

--- FSD-BACKEND-master/Day5/frontend/src/App.jsx
+++ FSD-BACKEND-master/Day5/frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Register from './components/Register'
 import View from './components/View'
+import Delete from './components/Delete'
+import Update from './components/Update'
 
 const App = () => {
   return (
@@ -8,6 +10,8 @@ const App = () => {
   <h1 style={{backgroundColor:'purple',alignItems:'center',textAlign:'center',color:'white'}}>User Registrstion System</h1>
       <Register/>
       <View/>
+      <Delete/>
+      <Update/>
     </div>
   )
 }
